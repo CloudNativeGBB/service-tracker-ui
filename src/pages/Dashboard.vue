7603e8d780f76bd3c0dd9598bfd71ac24371ee39
Joey Schluchter <jschluchter@gmail.com>
2020-09-18 15:03:26 -0400

    Update Dashboard.vue

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row">
           <div class="col-md-6 col-xl-3">
-            <h6>CURRENT SERVICE STATUS (Azure Arc Demo)</h6>
+            <h6>STATUS (Azure Arc Demo)</h6>
           </div>
     </div>
 
